e6f4d97e34a74ed7b03cb4887fd8549388c4e6e0
daniloeberli <danilo.eberli@gmail.com>
2023-03-23 16:42:23 +0100

    feat: footer-middle structure

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
   import AppFooterTop from './components/AppFooterTop.vue';
+  import AppFooterMiddle from './components/AppFooterMiddle.vue';
   
 
   export default {
     components:{
       AppHeader,
       AppMain,
-      AppFooterTop
+      AppFooterTop,
+      AppFooterMiddle,
     }
   }
 </script>
@@ -18,6 +20,7 @@
     <AppHeader></AppHeader>
     <AppMain></AppMain>
     <AppFooterTop></AppFooterTop>
+    <AppFooterMiddle></AppFooterMiddle>
   
 </template>
 
---
+++ src/components/AppFooterMiddle.vue
@@ -0,0 +1,94 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <footer>
+        <div class="footer-middle-container">
+            <div class="middle-left">
+                <div>
+                    <div>
+                        <h2>DC comics</h2>
+                        <ul>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>shop</h2>
+                        <ul>
+                            <li><a href="#">Prova</a></li>
+                            <li><a href="#">Prova</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>DC</h2>
+                    <ul>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>SITES</h2>
+                    <ul>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                        <li><a href="#">Prova</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h2>hey</h2>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    height: 300px;
+    background-image: url(../assets/footer-bg.jpg);
+
+    .footer-middle-container {
+        width: 80%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        .middle-left{
+            display: flex;
+        }
+    }
+
+    h2{
+        color: white;
+    }
+
+    ul{
+        list-style: none;
+
+        a{
+            color: #3a6586;
+            text-decoration: none;
+        }
+    }
+}
+</style>
