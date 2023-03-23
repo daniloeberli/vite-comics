d72968bc753cecbdf20763272b66da2411583404
daniloeberli <danilo.eberli@gmail.com>
2023-03-23 15:45:02 +0100

    feat: Main component structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
   
 
   export default {
     components:{
-      AppHeader
+      AppHeader,
+      AppMain
     }
   }
 </script>
@@ -12,7 +14,7 @@
 <template>
   
     <AppHeader></AppHeader>
- 
+    <AppMain></AppMain>
   
 </template>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <main>
+        <div>
+            <h1>Content goes here</h1>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+main{
+    background-color: #1c1c1c;
+}
+
+div{
+    margin: 0 auto;
+    width: 80%;
+    height: 135px;
+    h1{
+        color: white;
+    }
+}
+
+</style>
