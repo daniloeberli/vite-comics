d05ec4ea32fd09ff670cd9e7319dced42af61c5d
daniloeberli <danilo.eberli@gmail.com>
2023-03-23 17:49:23 +0100

    fix: header color  & padding

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,53 +1,67 @@
 <script>
-   export default{
+export default {
 
-   }
+}
 </script>
 
 <template>
     <header>
         <div>
-        <img src="../assets/dc-logo.png" alt="logo dc">
-        <ul>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-            <li><a href="#">a</a></li>
-        </ul>
-    </div>
+            <img src="../assets/dc-logo.png" alt="logo dc">
+            <ul>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+                <li><a href="#">a</a></li>
+            </ul>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
-
-header{
-    background-color: pink;
+header {
+    
 }
 
-div{
-    padding: 20px 0;
+div {
     margin: 0 auto;
     width: 80%;
     display: flex;
     justify-content: space-between;
-    ul{
+
+    img{
+        padding: 20px 0;
+    }
+
+    ul {
         display: flex;
         align-items: center;
         list-style: none;
-        li{
-            a{
+
+        li {
+            height: 100%;
+            display: flex;
+            align-items: center;
+            &:hover{
+                    border-bottom: 5px solid #0282f9;
+                }
+
+            a {
                 display: inline-block;
                 padding: 0 15px;
                 text-decoration: none;
+                color: #464646;
+                &:hover{
+                    color: #0282f9;
+                }
             }
         }
     }
-}    
-
+}
 </style>
